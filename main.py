9a31957e257801cf1489c43393abed7d895f61a2
Kuan Yi Wang <kuanyi.wang0807@gmail.com>
2026-07-21 23:41:56 -0400

    Add: Window reading.

--- main.py
+++ main.py
@@ -1,28 +1,57 @@
+from AppKit import NSWorkspace
+
 from pathlib import Path
 from datetime import datetime
 import subprocess
 from pynput import keyboard
 
-output_dir = Path.home() / "Pictures" / "Screenshots"
+## Screen Read
+import Quartz
+
+### CONFIGS ###
+output_dir = Path.home() / "Downloads"
 output_dir.mkdir(parents=True, exist_ok=True)
 
-def take_screenshot():
-    filename = datetime.now().strftime("Screenshot-%Y%m%d-%H%M%S.png")
-    output = output_dir / filename
 
-    subprocess.run([
-        "/usr/sbin/screencapture",
-        "-i",  # click-and-drag selection
-        "-x",  # suppress sound
-        str(output),
-    ], check=True)
 
-    print(f"Saved: {output}")
+class WindowHelper():
+    def __init__(self):
+        self.options = (
+            Quartz.kCGWindowListOptionOnScreenOnly |
+            Quartz.kCGWindowListExcludeDesktopElements
+        )
 
-hotkeys = keyboard.GlobalHotKeys({
-    "<caps_lock>+s": take_screenshot,
-})
+        self.windows = Quartz.CGWindowListCopyWindowInfo(
+            self.options,
+            Quartz.kCGNullWindowID
+        )
 
-print("Press Ctrl+Option+S to select a screenshoSt. Ctrl+C to quit.")
-hotkeys.start()
-hotkeys.join()
+
+class SmartScreenshot():
+    def __init__(self):
+        self.hotkeys = keyboard.GlobalHotKeys({
+            "<caps_lock>+s": self.take_screenshot,
+        })
+        self.hotkeys.start()
+        self.hotkeys.join()
+
+    def find_app_name(self):
+
+
+    def take_screenshot(self):
+        filename = datetime.now().strftime("Screenshot-%Y%m%d-%H%M%S.png")
+        output = output_dir / filename
+
+        res = subprocess.run([
+            "/usr/sbin/screencapture",
+            "-i",  # click-and-drag selection
+            "-x",  # suppress sound
+            str(output),
+        ], 
+        check=True,
+        timeout=5)
+
+        self.process_screenshot()
+
+    def process_screenshot(self):
+
